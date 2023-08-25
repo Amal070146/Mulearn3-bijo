066d3b604fffcc4ffd3d5b7be50bfa1a76f5021a
bijomathewjose <bijomathewjose@gmail.com>
2023-08-25 21:15:23 +0530

    no scroll for certain time

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,38 @@ import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
 import { useLayoutEffect, useRef } from "react";
 import "./App.css";
+import rocketLevel1 from "./assets/level1.png"
+import rocketLevel2 from "./assets/level2.png"
+import rocketLevel3 from "./assets/level3.png"
+import rocketLevel4 from "./assets/level4.png"
 
 gsap.registerPlugin(ScrollTrigger);
 
 export default function Scene() {
   const component = useRef(null);
   const slider = useRef(null);
+  const rocket = useRef(null);
 
   useLayoutEffect(() => {
     const ctx = gsap.context(() => {
-      const panels = gsap.utils.toArray(".panel");
-      gsap.to(panels, {
-        xPercent: -100 * (panels.length - 1),
-        ease: "none",
+      gsap.to(slider.current, {
         scrollTrigger: {
           trigger: slider.current,
-          pin: true,
-          scrub: 1,
-          snap: 1 / (panels.length - 1),
-          end: () => "+=" + (slider.current as unknown as HTMLElement).offsetWidth,
-          markers: true
+          start: "top top",
+          end: "+=8000px",
+          scrub: true,
+          pin: true
         }
-      });
-    }, component);
+      })
+      gsap.to(rocket.current, {
+        scrollTrigger: {
+          trigger: slider.current,
+          start: "top top",
+          end: "bottom top",
+          
+        }
+      })
+    })
     return () => ctx.revert();
   });
 
@@ -35,18 +44,12 @@ export default function Scene() {
         <h2>First Container</h2>
       </div>
       <div ref={slider} className="container">
-        <div className="description panel blue">
-          <div>
-            SCROLL DOWN
-            <div className="scroll-down">
-              <div className="arrow"></div>
-            </div>
-          </div>
+        <div ref={rocket} className="rocket">
+          <img src={rocketLevel4} alt="rocket" className="rocket-parts"/>
+          <img src={rocketLevel3} alt="rocket" className="rocket-parts"/>
+        <img src={rocketLevel2} alt="rocket" className="rocket-parts"/>
+        <img src={rocketLevel1} alt="rocket" className="rocket-parts"/>
         </div>
-
-        <div className="panel red">ONE</div>
-        <div className="panel orange">TWO</div>
-        <div className="panel purple">THREE</div>
       </div>
       <div className="lastContainer">Last Container</div>
     </div>
