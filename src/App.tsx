bee3163af292eb17a0a586d49ab0d13f9fca4b80
bijomathewjose <bijomathewjose@gmail.com>
2023-08-26 01:34:46 +0530

    again fall of added need to improve

--- src/App.tsx
+++ src/App.tsx
@@ -13,14 +13,15 @@ export default function Scene() {
   const component = useRef(null);
   const slider = useRef(null);
   const rocket = useRef(null);
-  const level1FallOf= useRef(null);
+  const level1FallOf= useRef(null),level2FallOf= useRef(null),level3FallOf= useRef(null),level4FallOf= useRef(null);
 
   useLayoutEffect(() => {
     const ctx = gsap.context(() => {
       gsap.to(rocket.current, {
-        y: 0,
-        x:'70vw',  
-        rotate: 180,
+        x: '100vw',
+        y: '0vh',
+        scale: 0.5,
+        rotate: 100,
         ease: "ease.in",
           scrollTrigger: {
             trigger: slider.current,
@@ -30,31 +31,60 @@ export default function Scene() {
             pin: true,
         }
       })
-      gsap.from(level1FallOf.current, {
-        y: 0,
-      
-        rotate: 0,
-        scale: 1,
-        ease: "move.out",
-          scrollTrigger: {
-            trigger: rocket.current,
-            start: "top top",
-            end: "bottom center",
-            scrub: 1,
-            pin: true,
-        }
-      })
+
       gsap.to(level1FallOf.current, {
-        y: 0,
-        x:'00vw',  
+        x:'100vw',
+        y: '0',  
         rotate: 360,
         scale: 0,
         ease: "bounce.out",
           scrollTrigger: {
             trigger: rocket.current,
             start: "top top",
-            end: "bottom center",
+            end: "+=100",
             scrub: 1,
+            pin: true,
+        }
+      })
+      gsap.to(level2FallOf.current, {
+        x:'100vw',
+        y: '0',  
+        rotate: 60,
+        scale: 0,
+        ease: "bounce.out",
+          scrollTrigger: {
+            trigger: rocket.current,
+            start: "top top",
+            end: "+=200",
+            scrub: 1,
+            pin: true,
+        }
+      })
+      gsap.to(level3FallOf.current, {
+        x:'100vw',
+        y: '0',  
+        rotate: 800,
+        scale: 0,
+        ease: "bounce.out",
+          scrollTrigger: {
+            trigger: rocket.current,
+            start: "+=0",
+            end: "+=300",
+            scrub: 2,
+            pin: true,
+        }
+      })
+      gsap.to(level4FallOf.current, {
+        x:'80vw',
+        y: '0',  
+        rotate: -80,
+        scale: 0,
+        ease: "bounce.out",
+          scrollTrigger: {
+            trigger: rocket.current,
+            start: "+=300",
+            end: "+=500",
+            scrub: 2,
             pin: true,
         }
       })
@@ -71,9 +101,9 @@ export default function Scene() {
       </div>
       <div ref={slider} className="container">
         <div ref={rocket} className="rocket">
-          <img src={rocketLevel4} alt="rocket" className="rocket-parts"/>
-          <img src={rocketLevel3} alt="rocket" className="rocket-parts"/>
-        <img src={rocketLevel2} alt="rocket" className="rocket-parts"/>
+          <img src={rocketLevel4} ref={level4FallOf} alt="rocket" className="rocket-parts"/>
+          <img src={rocketLevel3} ref={level3FallOf} alt="rocket" className="rocket-parts"/>
+        <img src={rocketLevel2}  ref={level2FallOf} alt="rocket" className="rocket-parts"/>
         <img src={rocketLevel1} ref={level1FallOf} alt="rocket" className="rocket-parts"/>
         </div>
       </div>
