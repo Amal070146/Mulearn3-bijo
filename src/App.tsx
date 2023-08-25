63b8f584bcba3bbc62da5e7baf59cdf9369ffc03
bijomathewjose <bijomathewjose@gmail.com>
2023-08-26 01:17:32 +0530

    some progress on rotation detachment while moving not great but there is progress

--- src/App.tsx
+++ src/App.tsx
@@ -13,29 +13,55 @@ export default function Scene() {
   const component = useRef(null);
   const slider = useRef(null);
   const rocket = useRef(null);
+  const level1FallOf= useRef(null);
 
   useLayoutEffect(() => {
     const ctx = gsap.context(() => {
-      gsap.to(slider.current, {
-        scrollTrigger: {
-          trigger: slider.current,
-          start: "top top",
-          end: "+=8000px",
-          scrub: true,
-          pin: true
+      gsap.to(rocket.current, {
+        y: 0,
+        x:'70vw',  
+        rotate: 180,
+        ease: "ease.in",
+          scrollTrigger: {
+            trigger: slider.current,
+            start: "top top",
+            end: "+=500",
+            scrub: 1,
+            pin: true,
         }
       })
-      gsap.to(rocket.current, {
-        scrollTrigger: {
-          trigger: slider.current,
-          start: "top top",
-          end: "bottom top",
-          
+      gsap.from(level1FallOf.current, {
+        y: 0,
+      
+        rotate: 0,
+        scale: 1,
+        ease: "move.out",
+          scrollTrigger: {
+            trigger: rocket.current,
+            start: "top top",
+            end: "bottom center",
+            scrub: 1,
+            pin: true,
+        }
+      })
+      gsap.to(level1FallOf.current, {
+        y: 0,
+        x:'00vw',  
+        rotate: 360,
+        scale: 0,
+        ease: "bounce.out",
+          scrollTrigger: {
+            trigger: rocket.current,
+            start: "top top",
+            end: "bottom center",
+            scrub: 1,
+            pin: true,
         }
       })
     })
+      
     return () => ctx.revert();
-  });
+  },[slider])
 
   return (
     <div className="App" ref={component}>
@@ -48,10 +74,10 @@ export default function Scene() {
           <img src={rocketLevel4} alt="rocket" className="rocket-parts"/>
           <img src={rocketLevel3} alt="rocket" className="rocket-parts"/>
         <img src={rocketLevel2} alt="rocket" className="rocket-parts"/>
-        <img src={rocketLevel1} alt="rocket" className="rocket-parts"/>
+        <img src={rocketLevel1} ref={level1FallOf} alt="rocket" className="rocket-parts"/>
         </div>
       </div>
-      <div className="lastContainer">Last Container</div>
+      <div  className="lastContainer">Last Container</div>
     </div>
   );
 }
