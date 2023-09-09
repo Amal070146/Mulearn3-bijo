0c0bb525e5e597c22c6891c0d9589306ca502972
bijomathewjose <bijomathewjose@gmail.com>
2023-09-09 21:52:01 +0530

    update

--- src/components/Frame.tsx
+++ src/components/Frame.tsx
@@ -1,8 +1,8 @@
 import { desc as description } from "./support";
 import './Frame.css'
-const Frame = ({level,desc=description,refer}:{level:string,desc?:string,refer:null|React.RefObject<HTMLDivElement>}) => {
+const Frame = ({level,desc=description,refer,classname}:{level:string,desc?:string,refer:null|React.RefObject<HTMLDivElement>,classname:string}) => {
     return (
-        <div className="frame" ref={refer}>
+        <div className={`frame ${classname}`} ref={refer}>
             <div className="text-wrapper">Level {level}</div>
             <p className="basic-enablement">{desc}</p>
         </div>
--- src/components/Rocket.tsx
+++ src/components/Rocket.tsx
@@ -35,31 +35,11 @@ const Rocket = () => {
           end: "bottom top",
           markers: true,
           scrub: 1,
-          pin: true
+          pin: true,
         }
       })
-      
-      .fromTo(rocket.current,  
-        {
-        yPercent: '100',        
-      },{
-        yPercent: '0',
-      }).fromTo(levelPointer.current,{
-        xPercent: 110,
-        yPercent: 600,
-        opacity: 0,
-      },{
-        xPercent: 110,
-        yPercent: 600,
-        opacity: 1,
-      },'+=10')
-      .fromTo(descLevel1.current ,{
-        xPercent: 200,
-        yPercent: 425,
-        opacity: 0,
-        scale: 1,
-      },{ opacity: 1, xPercent: 130, yPercent: 425, scale: 1,stagger: 0,ease: "ease.in(1.7)"})
     })
+
     return ()=>ctx.revert()
   },[])
 
@@ -70,33 +50,27 @@ const Rocket = () => {
       <div className='header-text'>Journey at µLearn</div>
     </div>
     <div className="rocket" ref={rocket}>
-      <Layers refer={rockerLayer4} image={layer4}/>
-      <Layers refer={rockerLayer3} image={layer3}/>
-      <Layers refer={rockerLayer2} image={layer2}/>
-      <Layers refer={rockerLayer1} image={layer1}/>
+      <img src={layer4} alt="layer4" className="layer" ref={rockerLayer4} />
+      <img src={layer3} alt="layer3" className="layer" ref={rockerLayer3} />
+      <img src={layer2} alt="layer2" className="layer" ref={rockerLayer2} />
+      <img src={layer1} alt="layer1" className="layer" ref={rockerLayer1} />
     </div>
-    <Frame level="1" refer={descLevel1}/>
-    <Frame level="2" refer={descLevel2}/>
-    <Frame level="3" refer={descLevel3}/>
-    <Frame level="4" refer={descLevel4}/>
-    <svg width='25%' viewBox="0 0 4080 748" fill="none" preserveAspectRatio='xMidYMid meet' ref={levelPointer} style={{position:"absolute"}}>
+    <Frame level="1" refer={descLevel1} classname="levels"/>
+    <svg width='25%' viewBox="0 0 4080 748" fill="none" preserveAspectRatio='xMidYMid meet' className='levelPointer1' ref={levelPointer} style={{position:"absolute"}}>
       <path d="M39.0769 38.4617L742.923 742.308H4079.69" stroke="white" strokeWidth="10"/>
       <circle cx="25.6418" cy="25.0266" r="25" fill="white"/>
     </svg>
+    
+    {/* 
+    <Frame level="2" refer={descLevel2}/>
+    <Frame level="3" refer={descLevel3}/>
+    <Frame level="4" refer={descLevel4}/>
     <img src={moon} alt="moon" className="moon planet" ref={moonRef}/>
-    <img src={earth} alt="earth" className="earth planet" ref={earthRef}/>
+    <img src={earth} alt="earth" className="earth planet" ref={earthRef}/> */}
   </div>
   )
 }
 
-
-const Layers=({refer,image}:{refer:null|React.RefObject<HTMLDivElement>,image:string})=>{
-  return (
-    <div className="layer" ref={refer}>
-      <img src={image} alt="layer4" className="layer4" />
-    </div>
-  );
-}
 
 
 export default Rocket
